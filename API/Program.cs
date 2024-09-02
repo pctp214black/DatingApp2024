25c26ca5542e1f0fe55d4da0a6047cbb25b87ff6
pctp214joker <pctp214joker@gmail.com>
2024-09-01 22:38:38 -0600

    Initial configuration

--- API/Program.cs
+++ API/Program.cs
@@ -4,11 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+/*
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();*/
 
 var app = builder.Build();
-
+/*
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,7 +20,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+*/
 app.MapControllers();
 
 app.Run();

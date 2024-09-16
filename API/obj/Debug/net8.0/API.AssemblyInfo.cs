aed904b7091045842cf7c723a6342d2c83513023
pctp214joker <pctp214joker@gmail.com>
2024-09-16 14:31:07 -0600

    create mkcert and ssl

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2bd8c062fb0d6a369f95129430be863ea6f616")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c964564b6dd3ea03e1fddb15bee4f81cd8e119a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

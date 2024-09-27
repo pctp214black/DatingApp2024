bc606b24e4830f30664de2a5fb7abb5b5673dc46
pctp214joker <pctp214joker@gmail.com>
2024-09-27 10:42:21 -0600

    Modified token. Add nav and home component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,26 +3,40 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor,NavComponent],
+  imports: [RouterOutlet, NgFor,NavComponent, BsDropdownModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
-  title = 'Cualquier cosa';
-  http=inject(HttpClient);
-  users:any
-  
+  http = inject(HttpClient);
+  private accountService = inject(AccountService);
+  title = 'Date me';
+  users: any;
+
   ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/v1/users").subscribe({
-      next:(response)=>{this.users=response},
-      error:(error)=>{console.error(error)},
-      complete:()=>{console.info("Request Completed")}
-    }
-      
-    );
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(): void {
+    const userString = localStorage.getItem("user");
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
+    this.http.get<Object>("https://localhost:5001/api/v1/users").subscribe({
+      next: (response) => { this.users = response; },
+      error: (error) => { console.log(error); },
+      complete: () => { console.log("Request completed!"); }
+    });
   }
 }

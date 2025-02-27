da5be3971ff3bf02cfe9e631b9778c1c3cf1f7b3
pctp214black <pctp214joker@gmail.com>
2025-02-26 23:05:58 -0600

    Revert "Merge branch 'master' of https://github.com/pctp214black/DatingApp2024 into Parcial04"
    This reverts commit 9fd34afc8d92d35df477b37d1386e6854cf47a05, reversing
    changes made to 169cb1c9324c572924bf35f170ca1a45b540d988.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169cb1c9324c572924bf35f170ca1a45b540d988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224e9d87fd095e182806953921edf74ff8086f7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

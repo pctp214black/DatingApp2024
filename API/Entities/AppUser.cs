578990d056b16686afdb09379c0356ed6f022522
pctp214black <pctp214joker@gmail.com>
2025-03-11 21:51:14 -0600

    Add user likes

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -28,5 +28,9 @@ public class AppUser
     public required string Country { get; set; }
     public List<Photo> Photos { get; set; } = [];
 
+    public List<UserLike> LikedByUsers { get; set; } = [];
+
+    public List<UserLike> LikedUsers { get; set; } = [];
+
     // public int GetAge() => BirthDay.CalculateAge();
 }
